a79d9764da9c862c1aec576016040744b9ec854a
Agathe Guillemot <agathe.guillemot@dataiku.com>
2020-10-22 16:42:14 +0200

    Ensure proper feature ranking for DkuVisualizer

--- python-lib/dku_error_analysis_tree_parsing/tree_parser.py
+++ python-lib/dku_error_analysis_tree_parsing/tree_parser.py
@@ -104,9 +104,15 @@ class TreeParser(object):
                 features[name] = {
                     'mean':  avg
                 }
+
         ranked_feature_ids = rank_features_by_error_correlation(self.error_model.feature_importances_)
-        unpreprocess_feature_name = lambda feature_idx: self.get_split_parameters(feature_list[feature_idx]).feature
-        ranked_features = list(map(unpreprocess_feature_name, ranked_feature_ids))
-        _, unique_ids = np.unique(ranked_features, return_index=True)
-        tree = InteractiveTree(df, target, [ranked_features[idx] for idx in unique_ids], features)
+        def get_unique_ranked_features(accumulated_list, current_value, seen_values=set()):
+            unprocessed_name = self.get_split_parameters(feature_list[current_value]).feature
+            if current_value not in seen_values:
+                accumulated_list.append(unprocessed_name)
+                seen_values.add(unprocessed_name)
+            return accumulated_list
+
+        ranked_features = list(reduce(get_unique_ranked_features, ranked_feature_ids),[])
+        tree = InteractiveTree(df, target, ranked_features, features)
         return tree
