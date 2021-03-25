a333b3c9212b514166013168bf41060cee27f59b
Agathe Guillemot <agathe.guillemot@dataiku.com>
2021-03-25 12:29:07 +0100

    Better tooltips Clean histogram code

--- resource/js/histograms.js
+++ resource/js/histograms.js
@@ -1,5 +1,5 @@
 'use strict';
-app.directive('tooltipHistogram', function(TreeUtils) {
+app.directive('tooltip', function(TreeUtils) {
     return {
         scope: true,
         restrict: "C",
@@ -8,23 +8,22 @@ app.directive('tooltipHistogram', function(TreeUtils) {
             const binIndex = parseInt(attr.binIndex);
             scope.globalData = attr.wholeData;
             const histData = scope.globalData ? scope.histDataWholeSet[attr.feature] : scope.histData[attr.feature];
-            const probaError = histData.target_distrib[TreeUtils.WRONG_PREDICTION];
-            if (probaError && probaError[binIndex]) {
-                scope.localError = probaError[binIndex] * 100;
-            } else {
-                scope.localError = 0;
-            }
-            scope.samples = [histData.count[binIndex],
-                            histData.count[binIndex]/scope.selectedNode.samples[0]*100];
+            const nrErrors = histData.target_distrib[TreeUtils.WRONG_PREDICTION] && histData.target_distrib[TreeUtils.WRONG_PREDICTION][binIndex] || 0;
+            scope.localError = [
+                nrErrors,
+                nrErrors*100/histData.count[binIndex],
+                nrErrors*100/TreeUtils.computeLocalError(scope.treeData[0])[1]
+            ];
+            scope.samples = [
+                histData.count[binIndex],
+                histData.count[binIndex]*100/scope.selectedNode.samples[0],
+                histData.count[binIndex]*100/scope.treeData[0].samples[0]
+            ];
             if (histData.bin_value) {
                 scope.binName = histData.bin_value[binIndex];
             } else {
                 scope.binName = `[${histData.bin_edge[binIndex]}, ${histData.bin_edge[binIndex+1]})`;
             }
-
-            d3.select(element[0].children[0])
-            .attr("width", 190)
-            .attr("height", 80);
         }
     };
 });
@@ -38,133 +37,60 @@ app.directive("histogram", function (Format, TreeUtils, $compile) {
                 .attr("width", "100%")
                 .attr("height", "100%");
 
-            const margin = {top: 15, bottom: 40, left: 30, right: 20},
+            const margin = {top: 15, bottom: 40, left: 40, right: 20},
                 width = histSvg.node().getBoundingClientRect().width - margin.left - margin.right,
                 height = 195 - margin.top - margin.bottom;
             
             histSvg = histSvg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+            // Add x & y axes
             const x = d3.scale.ordinal().rangeRoundBands([0, width], .2);
-            const y = d3.scale.linear().range([height, 0]);
             const xAxis = d3.svg.axis().scale(x).orient("bottom");
+            const xRange = feature.numerical ? $scope.histData[feature.name].mid : $scope.histData[feature.name].bin_value.slice(0,10)
+            x.domain(xRange);
+
+            const y = d3.scale.linear().range([height, 0]);
+            y.domain([0, 100]);
             const yAxis = d3.svg.axis().scale(y).orient("left");
             yAxis.ticks(5);
-            y.domain([0, 100]);
 
-            const addInteractions = function(groups, onWholeSet) {
-                groups.on("mouseenter", function(d) {
-                    histSvg.append("g")
-                    .classed("tooltip", true)
-                    .classed("tooltip-histogram", true)
-                    .attr("feature", feature.name)
-                    .attr("whole-data", onWholeSet)
-                    .attr("bin-index", d.idx)
-                    .call(function() {
-                        $compile(this[0])($scope);
-                    });
-                })
-                .on("mousemove", function(){
-                    let xPosition = d3.mouse(this)[0] + 20;
-                    let yPosition = d3.mouse(this)[1];
-                    const histogramDim = d3.select(".histogram-svg").node().getBoundingClientRect();
-                    const tooltipDim = d3.select(".tooltip-histogram").node().getBoundingClientRect();
-                    if (xPosition + 25 + tooltipDim.width > histogramDim.width) {
-                        xPosition -= 30 + tooltipDim.width;
+            histSvg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .classed("x-axis__text", true)
+            .text(xValue => Format.ellipsis(xValue, 10))
+            .attr("transform", "translate(-15,10) rotate(-45)")
+            .attr("dy", "1em");
+
+            histSvg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .selectAll("text")
+            .classed("y-axis__text", true)
+            .text(percentage => percentage + "%");
+
+            const computeHistData = function(global) {
+                const values = global ? $scope.histDataWholeSet[feature.name] : $scope.histData[feature.name];
+                const samples = (global ? $scope.treeData[0]: $scope.selectedNode).samples[0]
+                return xRange.map(function(x, idx) {
+                    idx = (!feature.numerical && global) ? values.bin_value.indexOf(x) : idx;
+                    const bar = {data: [], idx};
+                    let y0 = 0;
+                    for (const pred in values.target_distrib) {
+                        const predBinCount = values.target_distrib[pred][idx];
+                        if (!predBinCount) continue;
+                        const predBinPercentage = predBinCount*100/samples;
+                        bar.data.push({x, y: predBinPercentage, y0, pred});
+                        y0 += predBinPercentage;
                     }
-                    if (yPosition + 15 + tooltipDim.height > histogramDim.height) {
-                        yPosition -= (yPosition + tooltipDim.height) - histogramDim.height + 15;
-                    }
-                    d3.select(".tooltip-histogram").attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-                })
-                .on("mouseleave", function() {
-                    d3.select(".tooltip-histogram").remove();
+                    return bar;
                 });
-            }
-
-            const addGroupProperties = function(groups, wholeData) {
-                groups.selectAll("rect")
-                .data(d => d.data)
-                .enter()
-                .append("rect")
-                .attr("class", d => d.pred === TreeUtils.WRONG_PREDICTION ? "rect--error" : "rect--correct")
-                .attr("x", d => x(d.x) + (wholeData? x.rangeBand()/2 : 0))
-                .attr("y", d => y(d.y0 + d.y))
-                .attr("height", d => y(d.y0) - y(d.y0 + d.y))
-                .attr("width", wholeData ? 0 : x.rangeBand());
-
-                if (wholeData) {
-                    histSvg.selectAll(".histogram__bar").selectAll("rect").transition().duration(200).attr("width", x.rangeBand()/2);
-                    histSvg.selectAll(".histogram__bar_global").selectAll("rect").transition().duration(200).attr("width", x.rangeBand()/2);
-                }
             }
 
             function update(global) {
-                let predArray = $scope.selectedNode.probabilities.map(_ => _[0]);
-                const values = global ? $scope.histDataWholeSet[feature.name] : $scope.histData[feature.name];
-                const samples = (global ? $scope.treeData[0]: $scope.selectedNode).samples[0]
-                const data = [];
-                if (feature.numerical) {
-                    values.mid.forEach(function(mid, idx) {
-                        const bar = {data: [], idx};
-                        let y0 = 0;
-                        predArray.forEach(function(prediction) {
-                            if (values.target_distrib[prediction][idx]) {
-                                const height = values.target_distrib[prediction][idx]*100/samples;
-                                bar.data.push({
-                                    x: mid,
-                                    y: height,
-                                    y0: y0,
-                                    pred: prediction,
-                                    interval: `[${values.bin_edge[idx]}, ${values.bin_edge[idx+1]})`
-                                });
-                                y0 += height;
-                            }
-                        });
-                        data.push(bar);
-                    });
-                    x.domain(values.mid);
-                } else {
-                    $scope.histData[feature.name].bin_value.slice(0,10).forEach(function(bin_value, idx) { // TODO: useless slice?
-                        idx = global ? values.bin_value.indexOf(bin_value) : idx;
-                        const bar = {data: [], idx};
-                        let y0 = 0;
-                        predArray.forEach(function(prediction) {
-                            if (values.target_distrib[prediction][idx]) {
-                                const height = values.target_distrib[prediction][idx]*100/samples;
-                                bar.data.push({
-                                    x: bin_value,
-                                    y: height,
-                                    y0: y0,
-                                    pred: prediction
-                                });
-                                y0 += height;
-                            }
-                        });
-                        data.push(bar);
-                    });
-                    x.domain($scope.histData[feature.name].bin_value.slice(0,10));
-                }
-                histSvg.append("g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(xAxis)
-                    .selectAll("text")
-                    .classed("x-axis__text", true)
-                    .attr("transform", "translate(-15,10) rotate(-45)")
-                    .attr("dy", "1em");
-
-                histSvg.selectAll(".tick text").text(function(d) {
-                    return Format.ellipsis(d, 10);
-                });
-
-                histSvg.append("g")
-                    .attr("class", "y axis")
-                    .call(yAxis)
-                    .append("text")
-                    .classed("y-axis__text", true)
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 6)
-                    .attr("dy", ".71em");
+                const data = computeHistData(global);
 
                 // Create groups for each series, rects for each segment
                 const groups = histSvg.selectAll("g.bar")
@@ -173,8 +99,41 @@ app.directive("histogram", function (Format, TreeUtils, $compile) {
                 .append("g")
                 .attr("class", global ? "histogram__bar_global" : "histogram__bar");
 
-                addGroupProperties(groups, global);
-                addInteractions(groups, global);
+                // Add bars
+                groups.selectAll("rect")
+                .data(d => d.data)
+                .enter()
+                .append("rect")
+                .attr("class", d => d.pred === TreeUtils.WRONG_PREDICTION ? "rect--error" : "rect--correct")
+                .attr("x", d => x(d.x) + (global? x.rangeBand()/2 : 0))
+                .attr("y", d => y(d.y0 + d.y))
+                .attr("height", d => y(d.y0) - y(d.y0 + d.y))
+                .attr("width", global ? 0 : x.rangeBand());
+
+                if (global) {
+                    histSvg.selectAll(".histogram__bar").selectAll("rect").transition().duration(200).attr("width", x.rangeBand()/2);
+                    histSvg.selectAll(".histogram__bar_global").selectAll("rect").transition().duration(200).attr("width", x.rangeBand()/2);
+                }
+
+                // Add tooltips on bar hover
+                groups.on("mouseenter", function(d) {
+                    d3.select("#container-" + feature.name).append("div")
+                    .attr("id", "tooltip-" + feature.name)
+                    .attr("feature", feature.name)
+                    .attr("bin-index", d.idx)
+                    .attr("whole-data", global ? true : null)
+                    .classed("tooltip", true)
+                    .call(function() {
+                        $compile(this[0])($scope);
+                    })
+                })
+                .on('mousemove', function() {
+                    const topOffset = histSvg.node().getBoundingClientRect().top + d3.mouse(this)[1] - margin.top - 75;
+                    d3.select('#tooltip-' + feature.name).style("top", topOffset).style("left", margin.left + 30 + d3.mouse(this)[0]);
+                })
+                .on("mouseleave", function() {
+                    d3.select("#tooltip-" + feature.name).remove();
+                });
             }
 
             const unregister = $scope.$watch("histDataWholeSet." + feature.name, function(nv) {
@@ -193,16 +152,10 @@ app.directive("histogram", function (Format, TreeUtils, $compile) {
                 }
             });
             
-            $scope.$watch("selectedNode", function(nv) {
-                if (nv) {
-                    histSvg.selectAll("rect").remove();
-                    histSvg.selectAll("g").remove();
-                    update();
-                    if ($scope.seeGlobalChartData && $scope.histDataWholeSet[feature.name]) {
-                        update(true);
-                    }
-                }
-            })
+            update();
+            if ($scope.seeGlobalChartData && $scope.histDataWholeSet[feature.name]) {
+                update(true);
+            }
         }
     }
 });
