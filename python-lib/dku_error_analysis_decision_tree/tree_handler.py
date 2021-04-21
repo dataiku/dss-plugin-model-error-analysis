be375db1ca1fb4ae0a84fa4a2a69e4229bbd0a8c
Du Phan <du.phan@dataiku.com>
2021-04-21 15:59:23 +0200

    retrieve the proba threshold of doctor

--- python-lib/dku_error_analysis_decision_tree/tree_handler.py
+++ python-lib/dku_error_analysis_decision_tree/tree_handler.py
@@ -43,7 +43,9 @@ class TreeHandler(object):
         return summary[ErrorAnalyzerConstants.PRIMARY_MODEL_TRUE_ACCURACY] # TODO: compute proper value
 
     def set_error_analyzer(self, original_model_handler):
-        self.analyzer = DkuErrorAnalyzer(original_model_handler)
+        pred = original_model_handler.get_predictor()
+        probability_threshold = pred.params.model_perf.get('usedThreshold', None)
+        self.analyzer = DkuErrorAnalyzer(original_model_handler, probability_threshold=probability_threshold)
 
     def set_current_node_id(self, node_id):
         self.current_node_id = node_id
