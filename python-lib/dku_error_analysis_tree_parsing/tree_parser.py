9f0438970a90ee19fa376eff731ecf1c60a73dbe
Agathe Guillemot <agathe.guillemot@dataiku.com>
2021-04-22 00:23:48 +0200

    Ensure proper features can be plotted in webapp

--- python-lib/dku_error_analysis_tree_parsing/tree_parser.py
+++ python-lib/dku_error_analysis_tree_parsing/tree_parser.py
@@ -11,22 +11,31 @@ from mealy import ErrorAnalyzerConstants
 
 class TreeParser(object):
     class SplitParameters(object):
-        def __init__(self, node_type, feature, value=None, value_func=lambda threshold: threshold, uses_preprocessed_feature=False, force_others_on_right=False):
+        def __init__(self, node_type, chart_name, value=None, friendly_name=None, value_func=lambda threshold: threshold,
+                uses_preprocessed_feature=False, force_others_on_right=False):
             self.node_type = node_type
-            self.feature = feature
+            self.chart_name = chart_name
+            self.friendly_name = friendly_name
             self.value = value
             self.value_func = value_func
             self.uses_preprocessed_feature = uses_preprocessed_feature
             self.force_others_on_right = force_others_on_right
+
+        @property
+        def feature(self):
+            return self.friendly_name or self.chart_name
 
     def __init__(self, model_handler, error_model):
         self.model_handler = model_handler
         self.error_model = error_model
         self.preprocessed_feature_mapping = {}
         self.rescalers = []
+        self.num_features = set()
         self._create_preprocessed_feature_mapping()
 
     def _add_flag_missing_value_mapping(self, step):
+        if step.output_block_name == "num_flagonly":
+            self.num_features.add(step.feature)
         self.preprocessed_feature_mapping[step._output_name()] = self.SplitParameters(Node.TYPES.CAT, step.feature, [np.nan])
 
     # CATEGORICAL HANDLING
@@ -42,46 +51,53 @@ class TreeParser(object):
         impact_map = getattr(step.impact_coder, "_impact_map", getattr(step.impact_coder, "encoding_map", None)) # To handle DSS10 new implem
         for value in impact_map.columns.values:
             preprocessed_name = "impact:{}:{}".format(step.column_name, value)
-            display_name = "{} [{}]".format(step.column_name, value)
-            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, display_name, uses_preprocessed_feature=True)
+            friendly_name = "{} [{}]".format(step.column_name, value)
+            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, step.column_name, friendly_name=friendly_name, uses_preprocessed_feature=True)
 
     # NUMERICAL HANDLING
+    def _add_identity_mapping(self, original_name):
+        self.num_features.add(original_name)
+        self.preprocessed_feature_mapping[original_name] = self.SplitParameters(Node.TYPES.NUM, original_name)
+
     def _add_binarize_mapping(self, step):
+        self.num_features.add(step.in_col)
         self.preprocessed_feature_mapping["num_binarized:" + step._output_name()] = self.SplitParameters(Node.TYPES.NUM, step.in_col, step.threshold)
 
     def _add_quantize_mapping(self, step):
         bounds = step.r["bounds"]
         value_func = lambda threshold: float(bounds[int(threshold) + 1])
         preprocessed_name = "num_quantized:{0}:quantile:{1}".format(step.in_col, step.nb_bins)
+        self.num_features.add(step.in_col)
         self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, step.in_col, value_func=value_func)
 
     # VECTOR HANDLING
     def _add_unfold_mapping(self, step):
         for i in range(step.vector_length):
             preprocessed_name = "unfold:{}:{}".format(step.input_column_name, i)
-            display_name = "{} [element #{}]".format(step.input_column_name, i)
-            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, display_name, uses_preprocessed_feature=True)
+            friendly_name = "{} [element #{}]".format(step.input_column_name, i)
+            self.num_features.add(friendly_name)
+            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, friendly_name, uses_preprocessed_feature=True)
 
     # TEXT HANDLING
     def _add_hashing_vect_mapping(self, step, with_svd=False):
         prefix = "thsvd" if with_svd else "hashvect"
         for i in range(step.n_features):
             preprocessed_name = "{}:{}:{}".format(prefix, step.column_name, i)
-            display_name = "{} [text #{}]".format(step.column_name, i)
-            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, display_name, uses_preprocessed_feature=True)
+            friendly_name = "{} [text #{}]".format(step.column_name, i)
+            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, None, friendly_name=friendly_name, uses_preprocessed_feature=True)
 
     def _add_text_count_vect_mapping(self, step):
         for word in step.resource["vectorizer"].get_feature_names():
             preprocessed_name = "{}:{}:{}".format(step.prefix, step.column_name, word)
-            display_name = "{}: occurrences of {}".format(step.column_name, word)
-            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, display_name, value_func=lambda threshold: int(threshold), uses_preprocessed_feature=True)
+            friendly_name = "{}: occurrences of {}".format(step.column_name, word)
+            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, None, friendly_name=friendly_name, value_func=lambda threshold: int(threshold), uses_preprocessed_feature=True)
 
     def _add_tfidf_vect_mapping(self, step):
         vec = step.resource["vectorizer"]
         for word, idf in zip(vec.get_feature_names(), vec.idf_):
             preprocessed_name = "tfidfvec:{}:{:.3f}:{}".format(step.column_name, idf, word)
-            display_name = "{}: tfidf of {}".format(step.column_name, word)
-            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, display_name, uses_preprocessed_feature=True)
+            friendly_name = "{}: tfidf of {}".format(step.column_name, word)
+            self.preprocessed_feature_mapping[preprocessed_name] = self.SplitParameters(Node.TYPES.NUM, None, friendly_name=friendly_name, uses_preprocessed_feature=True)
 
     def _create_preprocessed_feature_mapping(self):
         for step in self.model_handler.get_pipeline().steps:
@@ -100,28 +116,24 @@ class TreeParser(object):
                 TextTFIDFVectorizerProcessor: self._add_tfidf_vect_mapping
             }.get(step.__class__, lambda step: None)(step)
 
-    def _get_split_parameters(self, preprocessed_name, threshold=None):
-        return self.preprocessed_feature_mapping.get(preprocessed_name, self.SplitParameters(Node.TYPES.NUM, preprocessed_name, threshold))
+    def _get_split_parameters(self, preprocessed_name):
+        # Numerical features can have no preprocessing performed on them ("kept as regular")
+        if preprocessed_name not in self.preprocessed_feature_mapping:
+            self._add_identity_mapping(preprocessed_name)
+        return self.preprocessed_feature_mapping[preprocessed_name]
 
     def build_tree(self, df, feature_list, preprocessed_x, target=DkuMEAConstants.ERROR_COLUMN):
-        num_features = {}
-        for name, settings in self.model_handler.get_preproc_handler().collector_data.get('per_feature').items():
-            avg = settings.get('stats').get('average')
-            if avg is not None: # TODO AGU: add cases where missing is not replaced by mean (ch49216)
-                num_features[name] = {
-                    'mean':  avg
-                }
-
         # Retrieve feature names without duplicates while keeping the ranking order
-        ranked_feature_ids = np.argsort(-self.error_model.feature_importances_)
-        ranked_feature_ids_unique, seen_values = [], set()
+        # TODO: allow rejected features as well?
+        ranked_feature_ids = np.argsort(- self.error_model.feature_importances_)
+        unique_ranked_feature_names, seen_values = [], set()
         for idx in ranked_feature_ids:
-            unprocessed_name = self._get_split_parameters(feature_list[idx]).feature
-            if unprocessed_name not in seen_values:
-                ranked_feature_ids_unique.append(unprocessed_name)
-                seen_values.add(unprocessed_name)
+            chart_name = self._get_split_parameters(feature_list[idx]).chart_name
+            if chart_name not in seen_values and chart_name is not None:
+                unique_ranked_feature_names.append(chart_name)
+                seen_values.add(chart_name)
 
-        tree = InteractiveTree(df, target, ranked_feature_ids_unique, num_features)
+        tree = InteractiveTree(df, target, unique_ranked_feature_names, self.num_features)
         self.parse_nodes(tree, feature_list, preprocessed_x)
         return tree
 
@@ -152,8 +164,7 @@ class TreeParser(object):
 
             feature_idx, threshold = features[node_id], thresholds[node_id]
             preprocessed_feature = feature_list[feature_idx]
-            split_parameters = self._get_split_parameters(preprocessed_feature, threshold)
-
+            split_parameters = self._get_split_parameters(preprocessed_feature)
             if split_parameters.uses_preprocessed_feature:
                 tree.df[split_parameters.feature] = preprocessed_x[:, feature_idx]
             if split_parameters.value is None:
