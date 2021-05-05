96124f1b019a7c62fa46941bf6c09d1fdf8c3798
Agathe Guillemot <agathe.guillemot@dataiku.com>
2021-05-05 20:21:53 +0200

    Add pytest.ini Rename integration test

--- tests/python/integration/test_tree_parser_unit.py
+++ tests/python/integration/test_tree_parser_unit.py
@@ -3,5 +3,5 @@ from dku_plugin_test_utils import dss_scenario
 
 TEST_PROJECT_KEY = "PLUGINTESTMODELERRORANALYSIS"
 
-def test_run_timeseries_forecast_regular(user_dss_clients):
+def test_run_parser_tests(user_dss_clients):
     dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="PARSERTESTS")
