c9cae2be7dbc3a8b8c6c04d87bda432deb4ed158
Agathe Guillemot <agathe.guillemot@dataiku.com>
2022-01-11 18:35:27 +0100

    Update code env requirements

--- python-lib/dku_error_analysis_model_parser/model_handler_utils.py
+++ python-lib/dku_error_analysis_model_parser/model_handler_utils.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import logging, sys
 from dku_error_analysis_utils import safe_str
-
 from dataiku.doctor.posttraining.model_information_handler import PredictionModelInformationHandler
 
-LOGGER = logging.getLogger(__name__)
 
 def get_model_handler(model, version_id=None):
     try:
         params = model.get_predictor(version_id).params
-        return PredictionModelInformationHandler(params.split_desc, params.core_params, params.model_folder, params.model_folder)
+        return PredictionModelInformationHandler(
+            params.split_desc, params.core_params, params.model_folder, params.model_folder
+        )
     except Exception as e:
-        from future.utils import raise_
         if "ordinal not in range(128)" in safe_str(e):
-            raise_(Exception, "Model Error Analysis requires models built with python3. This one is on python2.", sys.exc_info()[2])
+            raise Exception("Model stress test only supports models built with Python 3. This one was built with Python 2.") from None
         else:
-            raise_(Exception, "Fail to load saved model: {}".format(e), sys.exc_info()[2])
+            raise e
